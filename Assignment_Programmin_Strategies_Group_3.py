c8f5bb1c3940df28359c0d28bc3ff17739732b35
Devon <devon.huynh@edu.sait.ca>
2026-03-04 16:44:48 -0700

    receipts and indents fixed

--- Assignment_Programmin_Strategies_Group_3.py
+++ Assignment_Programmin_Strategies_Group_3.py
@@ -113,7 +113,7 @@ another_order = input("Would you like to order another drink? (Y/N): ".lower())
 #Reciept Summary
 if another_order == "n" or " ":
     print(border)
-    print(f"{'ORDER SUMMARY':^40}") #Lelandi
+    print(f"\tRECEIPT SUMMARY") #Lelandi
     print(border)
     print(f"\nNumber of Drinks:\t{order_count}"
           f"\nSubtotal:\t\t{(discount_before_subtotal):.2f}"
