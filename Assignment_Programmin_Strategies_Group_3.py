c89a531c55b897c7ec871a633a874e47f70ab1c6
Ben Gataric <benjamin.gataric@edu.sait.ca>
2026-03-04 17:35:46 -0700

    some small formatting changes

--- Assignment_Programmin_Strategies_Group_3.py
+++ Assignment_Programmin_Strategies_Group_3.py
@@ -1,5 +1,5 @@
 from typing import Final
-#Constants (TASK 1 - put constants in a dictionary or tuple - WHO?)
+#Constants - Keep as regular constants, placing in a dictionary will cause errors in the rest of the code
 COFFEE: Final = "Coffee"
 TEA: Final = "Tea"
 HOT_COCO: Final = "Hot Chocolate" #Lelandi
@@ -65,17 +65,17 @@ extra_order = {}
 extra_selection = int(input("Select extra (0-3): "))
 while extra_selection == 1 or 2 or 3 or 0:
     if extra_selection == 1:
-        print(f"{SUGAR} added ({SUGAR_PRICE})")
+        print(f"{SUGAR} added (+${SUGAR_PRICE:.2f})")
         extra_order[SUGAR] = SUGAR_PRICE
         extra_selection = int(input("Select extra (0-3): "))
         if extra_selection == 1:
             print("Sugar is already added.")
     elif extra_selection == 2:
-        print(f"{CREAM} added ({CREAM_PRICE})")
+        print(f"{CREAM} added (+${CREAM_PRICE:.2f})")
         extra_order[CREAM] = CREAM_PRICE
         extra_selection = int(input("Select extra (0-3): "))
     elif extra_selection == 3:
-        print(f"{SYRUP} added ({SYRUP_PRICE})")
+        print(f"{SYRUP} added (+${SYRUP_PRICE:.2f})")
         extra_order[SYRUP] = SYRUP_PRICE
         extra_selection = int(input("Select extra (0-3): "))
     elif extra_selection == 0:
@@ -123,52 +123,5 @@ if another_order.lower() == "n" or " ":
           f"\nTax (5%):\t\t{total_tax:.2f}"
           f"\nGrand Total:\t\t{Grand_total:.2f}")
     print(border)
-#Part 2 - WHO?
-
-
-'''Part 2: Multiple Orders and Final Receipt
-Update your Part 1 program to include the following functionality:
-
-1. Handling Multiple Orders:
-o Allow the user to place multiple beverage orders
-• After each order, ask: “Would you like to order another drink? (Y/N)” and allow
-placing a new order if the user wants.
-o Keep running totals for all orders
-o Track number of drinks ordered
-
-2. Displaying Final Receipt:
-o Display final receipt when user finishes ordering(DONE)
-o Show total number of drinks(DONE)
-o Show subtotal, tax, and grand total(DONE)
-o Display thank you message(DONE)
-
-3. Validating the User’s Input:
-o Validate all user inputs (beverage choice 1-3, extra choice 0-3)(DONE)
-• If the user input falls outside of the allowed values, display an appropriate
-message, and have them try again.(DONE)
-o Handle empty input when asking for another order. Empty input will be considered as
-the case of entering ‘n/N’(DONE)
-o Display appropriate error messages(DONE)
-o It is NOT necessary to verify that input values match the expected data type. In other
-words, you can assume that the user will enter digits for an integer item such as
-beverage and extra selection.(DONE)
-
-4. Constants: (DONE)
-o The program MUST use constants for:
-▪ Beverage names and prices
-▪ Extra names and prices
-▪ Discount threshold ($5.00) and rate (10%)
-▪ Tax rate (5%)
-
-5. Formatting:
-o Use formatted output with proper alignment (DONE)
-o Display prices with 2 decimal places (DONE)
-o Use separators and banners for visual organization(DONE)
-o Overall, the program's output must be formatted to exactly match the format, layout,
-and content displayed in the sample test runs provided in the Test Plan section. checkmarks for extras
-
-6. Using Compound Data:
-o The program must use compound data types to streamline your solution
-'''
-
-
+    print (f"\nThank you for visiting Python Cafe!"
+           f"\nWe hope to see you again soon!")
