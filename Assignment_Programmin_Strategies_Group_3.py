50d2f9f09c1748fbe4759e1d3631c4e61e4d6da6
Ben Gataric <benjamin.gataric@edu.sait.ca>
2026-03-06 02:00:04 -0700

    added a final customer order dictionary, placed in ordering system in a working loop, changed the extras menu to be in a dictionary to add the checkmark, added final customer order calculations, some re formatting

--- Assignment_Programmin_Strategies_Group_3.py
+++ Assignment_Programmin_Strategies_Group_3.py
@@ -15,123 +15,133 @@ SYRUP_PRICE: Final = .75
 DISCOUNT_THRESHOLD: Final = 5.00
 DISCOUNT: Final = .10
 TAX: Final = .05
-
-#Welcome Message
 border = f"="*37
-order_count = 1
+order_count = 0
+#Complete Customer Order
+final_customer_order = {}
+#Welcome Message
 print(border)
 print(f"\tWELCOME TO Python CAFE")
 print(border)
-print(f"\nOrder #{order_count}")
-print(f"-" * 8)
+while order_count >= 0:
+    order_count += 1
+    print(f"\nOrder #{order_count}")
+    print(f"-" * 8)
 
 #Beverage Menu
-print(border)
-print(f"\tBeverage Menu:")
-print(border)
-print(f"1. {COFFEE}\t\t${COFFEE_PRICE:.2f}"
-      f"\n2. {TEA}\t\t\t${TEA_PRICE:.2f}"
-      f"\n3. {HOT_COCO}\t${HOT_COCO_PRICE:.2f}")
-print(border)
+    print(f"\n{border}")
+    print(f"\tBeverage Menu:")
+    print(border)
+    print(f"1. {COFFEE}\t\t${COFFEE_PRICE:.2f}"
+          f"\n2. {TEA}\t\t\t${TEA_PRICE:.2f}"
+          f"\n3. {HOT_COCO}\t${HOT_COCO_PRICE:.2f}")
+    print(border)
 
 #Beverage selction
-user_selection = int(input("Your selection (1-3): "))
-drink_order = {}
-while user_selection == 1 or 2 or 3:
-    if user_selection == 1:
-        print("\nYou chose Coffee:")
-        drink_order[COFFEE] = COFFEE_PRICE
-        break
-    elif user_selection == 2:
-        print("\nYou chose Tea.")
-        drink_order[TEA] = TEA_PRICE
-        break
-    elif user_selection == 3:
-        print("\nYou chose Hot Chocolate.") #Lelandi
-        drink_order[HOT_COCO] = HOT_COCO_PRICE
-        break
-    else:
-        print("Please enter a number between 1 and 3.")
-        user_selection = int(input("Your selection (1-3): "))
-#Extra Selection
-
-#checkmark when choosing extras - WHO?
-print(f"\n{border}")
-print(f'{"Add Extras":>23}')
-print(border)
-print(f"1. {SUGAR}\t{SUGAR_PRICE:.2f}"
-      f"\n2. {CREAM}\t{CREAM_PRICE:.2f}"
-      f"\n3. {SYRUP}\t{SYRUP_PRICE:.2f}"
-      f"\n0. Finish order")
-print(border)
-
-#Extras Order
-extra_order = {}
-extra_selection = int(input("Select extra (0-3): "))
-
-while extra_selection != 0:
-    if extra_selection == 1:   
-        if SUGAR in extra_order:
-            print("Sugar is already added.")
+    user_selection = int(input("Your selection (1-3): "))
+    drink_order = {}
+    while user_selection == 1 or 2 or 3:
+        if user_selection == 1:
+            print("\nYou chose Coffee:")
+            drink_order[COFFEE] = COFFEE_PRICE
+            break
+        elif user_selection == 2:
+            print("\nYou chose Tea.")
+            drink_order[TEA] = TEA_PRICE
+            break
+        elif user_selection == 3:
+            print("\nYou chose Hot Chocolate.") #Lelandi
+            drink_order[HOT_COCO] = HOT_COCO_PRICE
+            break
         else:
-            print(f"{SUGAR} added (+${SUGAR_PRICE:.2f})")
-            extra_order[SUGAR] = SUGAR_PRICE
-    elif extra_selection == 2:
-        if CREAM in extra_order:
-            print("Cream is already added.")
-        else:
-            print(f"{CREAM} added (+${CREAM_PRICE:.2f})")
-            extra_order[CREAM] = CREAM_PRICE
-    elif extra_selection == 3:
-        if SYRUP in extra_order:
-            print("Syrup is already added.")
-        else:
-            print(f"{SYRUP} added (+${SYRUP_PRICE:.2f})")
-            extra_order[SYRUP] = SYRUP_PRICE
-    else:
-        print("Please enter a number between 0 and 3.")  
+            print("Please enter a number between 1 and 3.")
+            user_selection = int(input("Your selection (1-3): "))
+    #Extra Selection
+    extra_menu = {"1. Sugar": "$0.10", "2. Cream": "$0.50", "3. Syrup": "$0.75"}
+    print(f"\n{border}")
+    print(f'{"Add Extras":>23}')
+    print(border)
+    for key, val in extra_menu.items():
+        print(f"{key}\t{val}")
+    print(border)
+
+    #Extras Order
+    extra_order = {}
     extra_selection = int(input("Select extra (0-3): "))
 
-#Calculations
-subtotal = 0
-for v in drink_order.values():
-    subtotal += v
-for v in extra_order.values():
-    subtotal += v
-if subtotal > DISCOUNT_THRESHOLD:
-    discount_order = subtotal * DISCOUNT
-else:
-    discount_order = 0
-discount_before_subtotal = subtotal - discount_order
-total_tax = discount_before_subtotal * TAX
-Grand_total = total_tax + discount_before_subtotal
+    while extra_selection != 0:
+        if extra_selection == 1:   
+            if SUGAR in extra_order:
+                print("Sugar is already added.")
+            else:
+                print(f"{SUGAR} added (+${SUGAR_PRICE:.2f})")
+                extra_menu["1. Sugar"] = "$0.10 ✓"
+                extra_order[SUGAR] = SUGAR_PRICE
+        elif extra_selection == 2:
+            if CREAM in extra_order:
+                print("Cream is already added.")
+            else:
+                print(f"{CREAM} added (+${CREAM_PRICE:.2f})")
+                extra_order[CREAM] = CREAM_PRICE
+                extra_menu["2. Cream"] = "$0.50 ✓"
+        elif extra_selection == 3:
+            if SYRUP in extra_order:
+                print("Syrup is already added.")
+            else:
+                print(f"{SYRUP} added (+${SYRUP_PRICE:.2f})")
+                extra_order[SYRUP] = SYRUP_PRICE
+                extra_menu["3. Syrup"] = "$0.75 ✓"
+        else:
+            print("Please enter a number between 0 and 3.")  
+        extra_selection = int(input("Select extra (0-3): "))
+        
 
-#Order Summary
-print(border)
-print(f"\tORDER SUMMARY")
-print(border)
-for key, val in drink_order.items():
-    print(f"Beverage: {key}\t${val:.2f}")
-if extra_order != {}:
-    print(f"Extras:")
-    for key, val in extra_order.items():
-        print(f"   • {key}\t\t${val:.2f}")
-print(f"Subtotal: \t\t${subtotal:.2f}")
-if discount_order > 0:
-    print(f"Discount (10%)\t\t-${discount_order:.2f}"
-        f"\nAfter Discount:\t\t${discount_before_subtotal:.2f}")
-print(border)
-another_order = input("Would you like to order another drink? (Y/N): ")
+    #Oder Calculations
+    subtotal = 0
+    for v in drink_order.values():
+        subtotal += v
+    for v in extra_order.values():
+        subtotal += v
+    if subtotal > DISCOUNT_THRESHOLD:
+        discount_order = subtotal * DISCOUNT
+    else:
+        discount_order = 0
+    discount_before_subtotal = subtotal - discount_order
+    final_customer_order[order_count] = discount_before_subtotal
 
-#Reciept Summary
-if another_order.lower() == "n" or " ":
+    #Order Summary
+    print(f"\n{border}")
+    print(f"\tRDER SUMMARY")
     print(border)
-    print(f"\tRECEIPT SUMMARY") #Lelandi
+    for key, val in drink_order.items():
+        print(f"Beverage: {key}\t${val:.2f}")
+    if extra_order != {}:
+        print(f"Extras:")
+        for key, val in extra_order.items():
+            print(f"   • {key}\t\t${val:.2f}")
+    print(f"Subtotal: \t\t${subtotal:.2f}")
+    if discount_order > 0:
+        print(f"Discount (10%)\t\t-${discount_order:.2f}"
+            f"\nAfter Discount:\t\t${discount_before_subtotal:.2f}")
     print(border)
-    print(f"\nNumber of Drinks:\t{order_count}"
-          f"\nSubtotal:\t\t{(discount_before_subtotal):.2f}"
-          f"\nTax (5%):\t\t{total_tax:.2f}"
-          f"\nGrand Total:\t\t{Grand_total:.2f}")
-    print(border)
-    print (f"\nThank you for visiting Python Cafe!"
+    another_order = input(f"\nWould you like to order another drink? (Y/N): ")
+    #Recipt Calculations
+    grand_total = 0
+    for v in final_customer_order.values():
+        grand_total += v
+    grand_total_tax = grand_total * TAX
+    grand_total_final = grand_total + grand_total_tax
+
+    #Reciept Summary
+    if another_order.lower() != "y":
+        print(f"\n{border}")
+        print(f"\tRECEIPT SUMMARY") #Lelandi
+        print(border)
+        print(f"\nNumber of Drinks:\t{order_count}"
+              f"\nSubtotal:\t\t${grand_total:.2f}"
+              f"\nTax (5%):\t\t${grand_total_tax:.2f}"
+              f"\nGrand Total:\t\t${grand_total_final:.2f}")
+        print(border)
+        print (f"\nThank you for visiting Python Cafe!"
            f"\nWe hope to see you again soon!")
+        break
